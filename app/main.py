43401b3dbfb4e6f6e5f5ab8192c802ccb6de0b6d
Calvin Dougher <doughercalvin@gmail.com>
2024-11-20 18:11:48 -0800

    Port binding

--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
 from app import app
+import os
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    port = int(os.environ.get("PORT", 8000))  # Default to 8000 for local dev
+    uvicorn.run(app, host="0.0.0.0", port=port)
